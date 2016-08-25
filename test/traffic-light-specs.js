689bd9aa09d8fec4a7e2d1051dba9938e927fef8
Andy Tang <andyt@live.nl>
2016-08-25 22:35:10 +0200

    Start organizing your tests with `before` and `describe`

--- test/traffic-light-specs.js
+++ test/traffic-light-specs.js
@@ -2,21 +2,37 @@ import { expect } from 'chai';
 import { TrafficLight, COLORS } from '../src/traffic-light';
 
 describe('Traffic Lights', () => {
-  it('should turn the traffic lights to green', () => {
-    const trafficLight = new TrafficLight();
-    // Implement the test to check if the function `go`, sets the color to `GREEN`.
-    // Implement your test here
-    // Making sure that the color is not GREEN at the beginning
-    trafficLight.color = COLORS.RED;
-    trafficLight.go();
-    expect(trafficLight.color).to.equal(COLORS.GREEN);
+  before(() => {
+    // Instantiate a single instance of TrafficLight
   });
 
-  it('should turn the traffic lights to red', () => {
-    const trafficLight = new TrafficLight();
-    // Implement the test to check if the function `stop`, sets the color to `RED`.
-    // Implement your test here
-    trafficLight.stop();
-    expect(trafficLight.color).to.equal(COLORS.RED);
+  describe('When a car has been waiting for a red light for 10 seconds', () => {
+    before(() => {
+      // Move the setup code here
+    });
+
+    it('should turn the traffic lights to green', () => {
+      const trafficLight = new TrafficLight();
+      // Implement the test to check if the function `go`, sets the color to `GREEN`.
+      // Implement your test here
+      // Making sure that the color is not GREEN at the beginning
+      trafficLight.color = COLORS.RED;
+      trafficLight.go();
+      expect(trafficLight.color).to.equal(COLORS.GREEN);
+    });
+  });
+
+  describe('When the green light has been on for 10 seconds', () => {
+    before(() => {
+      // Move the setup code here
+    });
+
+    it('should turn the traffic lights to red', () => {
+      const trafficLight = new TrafficLight();
+      // Implement the test to check if the function `stop`, sets the color to `RED`.
+      // Implement your test here
+      trafficLight.stop();
+      expect(trafficLight.color).to.equal(COLORS.RED);
+    });
   });
 });
